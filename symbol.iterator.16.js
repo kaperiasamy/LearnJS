fd0644c07c80592d218e78b29231d23f960a274e
Peri <periasamy.kanagavel@outlook.com>
2025-07-19 07:27:38 +0530

    Symbol.iterator

---
+++ symbol.iterator.16.js
@@ -0,0 +1,32 @@
+const pns = {
+    lead: "Prn", 
+    engineer: "Venkatesh",
+    [Symbol.iterator]: function* () {
+        yield this.lead;
+        yield this.engineer;
+    }
+};
+
+const m2m = {
+    pns,
+    manager: "Yash", 
+    lead: "Ashwin",
+    engineer: "Harish"
+};
+/*
+function* pnsitem(team) {
+    yield team.lead; 
+    yield team.engineer;
+};
+*/
+function* m2miter(team) {
+    yield team.manager;
+    yield team.lead;
+    yield team.engineer;
+    yield* team.pns;
+};
+
+for(let name of m2miter(m2m))
+{
+    console.log(name);
+}
